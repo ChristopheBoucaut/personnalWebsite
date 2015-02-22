50f479891bf5b56b5c56333b8f3b9bc154b25173
Christophe Boucaut <boucaut.christophe@gmail.com>
2015-02-22 12:09:13 +0100

    Mise en page du niveau d'apprentissage des techno et ajout des grandes parties.

--- js/app/controllers/skillController.js
+++ js/app/controllers/skillController.js
@@ -5,7 +5,46 @@
         "$scope",
         function($scope) {
             var listSkills = {
-                "Type de compétences": [
+                "Développement backoffice": [
+                    {
+                        name: "skill 1",
+                        level: 1,
+                        favorites: false,
+                        inProgress: true
+                    },
+                    {
+                        name: "skill 2",
+                        level: 2,
+                        favorites: false,
+                        inProgress: true
+                    },
+                    {
+                        name: "skill 3",
+                        level: 3,
+                        favorites: true,
+                        inProgress: true
+                    }
+                ],
+                "Développement frontoffice": [
+                    {
+                        name: "skill 1",
+                        level: 4,
+                        favorites: false,
+                        inProgress: true
+                    },
+                    {
+                        name: "skill 2",
+                        level: 5,
+                        favorites: false,
+                        inProgress: true
+                    },
+                    {
+                        name: "skill 3",
+                        level: 5,
+                        favorites: true
+                    }
+                ],
+                "Développement mobile": [
                     {
                         name: "skill 1",
                         level: 5,
@@ -22,7 +61,51 @@
                         favorites: true
                     }
                 ],
-                "Type de compétences 2": [
+                "Travail en équipe": [
+                    {
+                        name: "SCRUM",
+                        level: 5,
+                        favorites: false
+                    },
+                    {
+                        name: "Trello",
+                        level: 2,
+                        favorites: false
+                    },
+                    {
+                        name: "Git",
+                        level: 5,
+                        favorites: true
+                    },
+                    {
+                        name: "Github",
+                        level: 5,
+                        favorites: true
+                    }
+                ],
+                "Devops": [
+                    {
+                        name: "SCRUM",
+                        level: 5,
+                        favorites: false
+                    },
+                    {
+                        name: "Trello",
+                        level: 2,
+                        favorites: false
+                    },
+                    {
+                        name: "Git",
+                        level: 5,
+                        favorites: true
+                    },
+                    {
+                        name: "Github",
+                        level: 5,
+                        favorites: true
+                    }
+                ],
+                "Divers": [
                     {
                         name: "skill 1",
                         level: 5,
@@ -38,7 +121,7 @@
                         level: 5,
                         favorites: true
                     }
-                ],
+                ]
             };
 
             $scope.listSkills = listSkills;
