8994efb534c1706db5a2d02fbc1f30af6a1ae20a
Christophe Boucaut <boucaut.christophe@gmail.com>
2015-03-07 22:46:48 +0100

    Fix bug load twitter collection.

--- js/app/controllers/contactController.js
+++ js/app/controllers/contactController.js
@@ -1,7 +1,16 @@
-"use strict";
-
 (function(angular) {
+    "use strict";
     angular.module("personnalWebsiteControllers").controller("ContactController", [
-        function() {}
+        function() {
+            (function(d, s, id) {
+                var js;
+                var fjs = d.getElementsByTagName(s)[0];
+                var p = /^http:/.test(d.location) ? "http" : "https";
+                js = d.createElement(s);
+                js.id = id;
+                js.src = p+"://platform.twitter.com/widgets.js";
+                fjs.parentNode.insertBefore(js,fjs);
+            })(document, "script", "twitter-wjs");
+        }
     ]);
 })(window.angular);
