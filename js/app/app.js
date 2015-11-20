5fcf2ddc8af6dd64a1dc78f3348267f9a022a76c
Christophe Boucaut <boucaut.christophe@gmail.com>
2015-11-20 23:01:06 +0100

    Replace use strict.

--- js/app/app.js
+++ js/app/app.js
@@ -1,6 +1,6 @@
-"use strict";
-
 (function(angular) {
+    "use strict";
+    
     var personnalWebsite = angular.module("personnalWebsite", [
         "ngRoute",
 
--- js/app/controllers.js
+++ js/app/controllers.js
@@ -1,5 +1,5 @@
-"use strict";
-
 (function(angular) {
+    "use strict";
+
     angular.module("personnalWebsiteControllers", []);
 })(window.angular);
